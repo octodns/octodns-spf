3530f03efda522558a8f56f42ab57a0d3541c3cc
Ross McFarland <rwmcfa1@gmail.com>
2023-08-20 16:38:59 -0700

    Full SPF field support

--- octodns_spf/__init__.py
+++ octodns_spf/__init__.py
@@ -2,6 +2,7 @@
 #
 #
 
+from io import StringIO
 from logging import getLogger
 
 from octodns.record import Record, RecordException
@@ -18,6 +19,48 @@ class SpfException(RecordException):
         super().__init__(msg)
 
 
+def _build_spf_value(
+    a_records,
+    mx_records,
+    ip4_addresses,
+    ip6_addresses,
+    includes,
+    exists,
+    soft_fail,
+):
+    buf = StringIO()
+    buf.write('v=spf1')
+    if a_records:
+        for a_record in a_records:
+            buf.write(' a:')
+            buf.write(a_record)
+    if mx_records:
+        for mx_record in mx_records:
+            buf.write(' mx:')
+            buf.write(mx_record)
+    if ip4_addresses:
+        for ip4_address in ip4_addresses:
+            buf.write(' ip4:')
+            buf.write(ip4_address)
+    if ip6_addresses:
+        for ip6_address in ip6_addresses:
+            buf.write(' ip6:')
+            buf.write(ip6_address)
+    if includes:
+        for include in includes:
+            buf.write(' include:')
+            buf.write(include)
+    if exists:
+        for exist in exists:
+            buf.write(' exist:')
+            buf.write(exist)
+    if soft_fail:
+        buf.write(' ~all')
+    else:
+        buf.write(' -all')
+    return buf.getvalue()
+
+
 class SpfSource(BaseSource):
     SUPPORTS_GEO = False
     SUPPORTS_DYNAMIC = False
@@ -25,11 +68,53 @@ class SpfSource(BaseSource):
 
     DEFAULT_TTL = 3600
 
-    def __init__(self, id, ttl=DEFAULT_TTL):
+    def __init__(
+        self,
+        id,
+        a_records=[],
+        mx_records=[],
+        ip4_addresses=[],
+        ip6_addresses=[],
+        includes=[],
+        exists=[],
+        soft_fail=False,
+        ttl=DEFAULT_TTL,
+    ):
         self.log = getLogger(f'SpfSource[{id}]')
-        self.log.debug('__init__: id=%s, ttl=%d', id, ttl)
+        self.log.info(
+            '__init__: id=%s, a_records=%s, mx_records=%s, ip4_addresses=%s, ip6_addresses=%s, includes=%s, exists=[], soft_fail=%s, ttl=%d',
+            id,
+            a_records,
+            mx_records,
+            ip4_addresses,
+            ip6_addresses,
+            includes,
+            exists,
+            soft_fail,
+            ttl,
+        )
         super().__init__(id)
+        self.a_records = a_records
+        self.mx_records = mx_records
+        # TODO: validate IPs?
+        self.ip4_addresses = ip4_addresses
+        self.ip6_addresses = ip6_addresses
+        self.includes = includes
+        self.exists = exists
+        self.soft_fall = soft_fail
+
         self.ttl = ttl
+
+        self.spf_value = _build_spf_value(
+            a_records,
+            mx_records,
+            ip4_addresses,
+            ip6_addresses,
+            includes,
+            exists,
+            soft_fail,
+        )
+        self.log.debug('__init__:   spf=%s', self.spf_value)
 
     def populate(self, zone, target=False, lenient=False):
         self.log.debug(
@@ -40,9 +125,6 @@ class SpfSource(BaseSource):
         )
 
         before = len(zone.records)
-
-        spf_value = 'v=spf1 -all'
-        self.log.debug('populate: spf value="%s"', spf_value)
 
         spf = None
         txt = None
@@ -66,12 +148,14 @@ class SpfSource(BaseSource):
                     )
             self.log.debug('populate:   adding our value, and replacing record')
             record = txt.copy()
-            record.values.append(spf_value)
+            record.values.append(self.spf_value)
             # replace with our updated version
             zone.add_record(record, lenient=lenient, replace=True)
         else:
             record = Record.new(
-                zone, '', {'ttl': self.ttl, 'type': 'TXT', 'value': spf_value}
+                zone,
+                '',
+                {'ttl': self.ttl, 'type': 'TXT', 'value': self.spf_value},
             )
             zone.add_record(record, lenient=lenient)
 
