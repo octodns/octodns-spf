6200e899c76d7cbfaeb09b79a811e6204cac12dc
Ross McFarland <rwmcfa1@gmail.com>
2023-09-10 14:39:35 -0700

    Add verification option to SpfSource

--- octodns_spf/processor.py
+++ octodns_spf/processor.py
@@ -57,10 +57,10 @@ class SpfDnsLookupProcessor(BaseProcessor):
         super().__init__(name)
 
     def _get_spf_from_txt_values(
-        self, record: Record, values: List[str]
+        self, fqdn: str, values: List[str]
     ) -> Optional[str]:
         self.log.debug(
-            f"_get_spf_from_txt_values: record={record.fqdn} values={values}"
+            f"_get_spf_from_txt_values: record={fqdn} values={values}"
         )
 
         # SPF values to validate will begin with 'v=spf1 '
@@ -73,7 +73,7 @@ class SpfDnsLookupProcessor(BaseProcessor):
         # More than one SPF value resolves as "permerror", https://datatracker.ietf.org/doc/html/rfc7208#section-4.5
         if len(spf) > 1:
             raise SpfValueException(
-                f"{record.fqdn} has more than one SPF value in the TXT record"
+                f"{fqdn} has more than one SPF value in the TXT record"
             )
 
         return spf[0]
@@ -89,13 +89,13 @@ class SpfDnsLookupProcessor(BaseProcessor):
         return values
 
     def _check_dns_lookups(
-        self, record: Record, values: List[str], lookups: int = 0
+        self, fqdn: str, values: List[str], lookups: int = 0
     ) -> int:
         self.log.debug(
-            f"_check_dns_lookups: record={record.fqdn} values={values} lookups={lookups}"
+            f"_check_dns_lookups: record={fqdn} values={values} lookups={lookups}"
         )
 
-        spf = self._get_spf_from_txt_values(record, values)
+        spf = self._get_spf_from_txt_values(fqdn, values)
 
         if spf is None:
             return lookups
@@ -105,12 +105,12 @@ class SpfDnsLookupProcessor(BaseProcessor):
         for term in terms:
             if lookups > 10:
                 raise SpfDnsLookupException(
-                    f"{record.fqdn} exceeds the 10 DNS lookup limit in the SPF record"
+                    f"{fqdn} exceeds the 10 DNS lookup limit in the SPF record"
                 )
 
             if term.startswith('ptr'):
                 raise SpfValueException(
-                    f"{record.fqdn} uses the deprecated ptr mechanism"
+                    f"{fqdn} uses the deprecated ptr mechanism"
                 )
 
             # These mechanisms cost one DNS lookup each
@@ -122,9 +122,7 @@ class SpfDnsLookupProcessor(BaseProcessor):
                 domain = term[len('include:') :]
                 answer = dns.resolver.resolve(domain, 'TXT')
                 answer_values = self._process_answer(answer)
-                lookups = self._check_dns_lookups(
-                    record, answer_values, lookups
-                )
+                lookups = self._check_dns_lookups(fqdn, answer_values, lookups)
 
         return lookups
 
@@ -136,6 +134,6 @@ class SpfDnsLookupProcessor(BaseProcessor):
             if record._octodns.get('lenient'):
                 continue
 
-            self._check_dns_lookups(record, record.values, 0)
+            self._check_dns_lookups(record.fqdn, record.values, 0)
 
         return zone
