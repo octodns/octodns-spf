1bb3090e68c662a4fc2053d63f26b3cac23c479c
Ross McFarland <rwmcfa1@gmail.com>
2023-11-11 12:41:45 -0800

    Add __version__ and deprecate __VERSION__ to line up with pep8 recommendation

--- octodns_spf/__init__.py
+++ octodns_spf/__init__.py
@@ -5,7 +5,8 @@
 from .processor import SpfDnsLookupProcessor
 from .source import SpfSource
 
-__VERSION__ = '0.0.2'
+# TODO: remove __VERSION__ with the next major version release
+__version__ = __VERSION__ = '0.0.2'
 
 # Quell warnings
 SpfDnsLookupProcessor
